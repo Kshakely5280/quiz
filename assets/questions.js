8b002d0bba31287643bcb0e280c55a70b661c4be
Kyle Shakely <kyleshakely@yahoo.com>
2023-06-07 16:06:31 -0600

    gave moved questions out of script logic

---
+++ assets/questions.js
@@ -0,0 +1,45 @@
+var quizData = [
+    {
+      questions: "Commonly used data types DO NOT include:",
+      a: "Strings",
+      b: "Booleans",
+      c: "Alerts",
+      d: "Numbers",
+      correct: "c",
+    },
+    {
+      questions:
+        "the condition in an if / else statement is encclosed within ___.",
+      a: "Quotes",
+      b: "Curly Brackets",
+      c: "parenthesis",
+      d: "Square Brackets",
+      correct: "c",
+    },
+    {
+      questions: "Arrays in JavaScript can be used to store ____.",
+      a: "Numbers and Strings",
+      b: "Other arrays",
+      c: "Booleans",
+      d: "All of the above",
+      correct: "d",
+    },
+    {
+      questions:
+        "String values must be enclosed within ___ when being assigned to variables",
+      a: "Commas",
+      b: "Curly Brackets",
+      c: "Quotes",
+      d: "Parenthesis",
+      correct: "c",
+    },
+    {
+      questions:
+        "A very useful tool used during development and debugging for printing content to the debugger is",
+      a: "Javascript",
+      b: "Terminal",
+      c: "For loops",
+      d: "console log",
+      correct: "d",
+    },
+  ];
